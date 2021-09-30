a1784f6d8ad8ccb6a73b93f15e1d40746f69d79e
Nikhil Poonia <45782973+nikhil12334@users.noreply.github.com>
2021-09-30 10:40:58 +0530

    Add files via upload

---
+++ referencingjsx.js
@@ -0,0 +1,28 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+function getButtonText() {
+  return "Click on me!";
+}
+
+
+const App = () => {
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ section1_basic.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+
+const App = () => {
+  return <div>Hi there!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
