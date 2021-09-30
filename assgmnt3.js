11e659e6027df357ba3a45dd41691a5f9ca9112d
Nikhil Poonia <45782973+nikhil12334@users.noreply.github.com>
2021-09-30 10:32:53 +0530

    Add files via upload

---
+++ assgmnt3.js
@@ -0,0 +1,35 @@
+const randomNumber=Math.random();
+
+if(randomNumber>=0.7){
+
+alert('greater or equal to 0.7');
+
+}
+
+else{
+
+alert('not greater');
+
+}
+
+const numbers=[1,2,3,4,5,6];
+
+for(let i=0;i<numbers.length;i++){
+
+console.log(numbers[i]);
+
+}
+
+const randomNumber2=Math.random();
+
+console.log(randomNumber);
+
+console.log(ramdomNumber2);
+
+if((randomNumber>0.7 && randomNumber2>0.7)||randomNumber<=0.2 || randomNumber2<=0.2))
+
+{
+
+alert('greater than 07 or smaller than 0.2');
+
+}
---
+++ assgmnt5.js
@@ -0,0 +1,21 @@
+const task1=document.ElementById('task-1');
+
+const task2=documentquerySelector('li');
+
+task1.style.color='white';
+
+task2.style.backgroundcolor='black';
+
+const docTitle=document.querySelector('title');
+
+docTitle.textContent='Assignment-Solved!';
+
+const docHead=documenthead;
+
+const docTitle2=document.head.querySelector('title');
+
+docTitle2.textContent='Assignment solved!';
+
+const h1=document.getElementsByTagName('h1');
+
+h1[0].textContent='Assignment-solved!';
